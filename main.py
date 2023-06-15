f3a355f1fae1a8d0064a9869b7bb2d8b5dba370d
truemerci <danilgladkiy7@gmail.com>
2023-06-15 14:42:23 +0300

    Correction of code logic and classes

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 class Unit:
     def __init__(self, health, damage, defense, critical_power, percent_cp):
-        self.health = health
-        self.damage = damage
-        self.defense = defense
-        self.critical_power = critical_power
-        self.percent_cp = percent_cp
+        self._health = health
+        self._damage = damage
+        self._defense = defense
+        self._critical_power = critical_power
+        self._percent_cp = percent_cp
 
     def __str__(self):
-        characteristic = f"Health: {self.health}, damage: {self.damage}, defense: {self.defense}, " \
-                         f"critical power: {self.critical_power}, percent critical power: {self.percent_cp}% "
+        characteristic = f"Health: {self._health}, damage: {self._damage}, defense: {self._defense}, " \
+                         f"critical power: {self._critical_power}, percent critical power: {self._percent_cp}%"
         return characteristic
 
 
 class Warrior(Unit):
-    pass
+    def __init__(self):
+        super().__init__(health=100, damage=50, defense=50, critical_power=100, percent_cp=15)
 
 
 class Mage(Unit):
-    pass
+    def __init__(self):
+        super().__init__(health=100, damage=70, defense=20, critical_power=140, percent_cp=10)
 
 
 class Archer(Unit):
-    pass
-
-
-warrior = Warrior(100, 50, 50, 100, 15)
-mage = Mage(100, 70, 20, 140, 8)
-archer = Archer(100, 60, 15, 120, 50)
+    def __init__(self):
+        super().__init__(health=100, damage=60, defense=10, critical_power=120, percent_cp=35)
